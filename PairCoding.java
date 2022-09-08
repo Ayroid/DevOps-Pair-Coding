fe991cb4a2ae6889ff50d1ca8eea546a48435763
Ayroid <ayushsk0000@gmail.com>
2022-09-08 21:00:43 +0530

    Added Main Function and Menu

--- PairCoding.java
+++ PairCoding.java
@@ -1,1 +1,34 @@
+import java.util.*;
+public class PairCoding{
+    Scanner sc = new Scanner(System.in);
+    public void primeNumber(){
+        System.out.print("Enter a number: ");
+        int num=sc.nextInt();
+        int factors=0;
+        for(int i=1;i<=num;i++){
+            if(num%i==0){
+                factors++;
+            }
+        }
+        if(factors==2){
+            System.out.println("Prime Number");
+        }
+        else{
+            System.out.println("Not Prime Number");
+        }
+    }
+    
+    public static void main(String[] args) {
+        PairCoding pc = new PairCoding();
+        System.out.println("Enter:\n1- Prime Number\n2- Multiplication Table");
+        int choice = pc.sc.nextInt();
+        switch(choice){
+            case 1:
+            pc.primeNumber();
+            break;
 
+            default:
+            System.out.println("Wrong Choice");
+        }
+    }
+}
